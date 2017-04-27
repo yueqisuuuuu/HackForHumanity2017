409409fda450d5efca521cf2b4cfc2be547d709b
Yueqi Su <ysu@scu.edu>
2017-04-26 22:53:18 -0700

    account page format

--- js/account.js
+++ js/account.js
@@ -1,32 +1,38 @@
-<div id="stuff">
-<span id="name"> Zack</span>
-<button onclick="changeName()"> Change Name </button>
-</div>
 
-<script>
-
-function saveName(c, a){
-    if (c==a){
-        var o = document.getElementById("name");
-        o.innerHTML="";
-    }
+/*
+function saveName(){
+    var newName =  document.getElementByClassName("newName").innerHTML;
+    var save = document.getElementById("name");
+    save.innerHTML = "";
+    save.innerHTML = newName;
 }
 
 function changeName(){
-    var l = document.getElementById("name");
-    var a = document.getElementById("name").innerHTML;
-    var n = document.createElement("input");
-    n.value=a;
-    l.innerHTML="";
-    l.appendChild(n);
-    var c = n.value;
-    var b = document.createElement ("button");
-    b.innerHTML="save";
-    b.onclick="saveName(c, a)";
-    l.appendChild(b);
-    
+    var oldName = document.getElementById("name");
+    var name = document.getElementById("name").innerHTML;
+    var nameInput = document.createElement("input");
+    nameInput.className = "newName";
+    nameInput.value = name;
+    oldName.innerHTML = "";
+    oldName.appendChild(nameInput);
+    var saveButton = document.createElement ("button");
+    saveButton.innerHTML = "save";
+    saveButton.className = "saveButton";
+    oldName.appendChild(saveButton);
+    document.getElementByClassName("saveButton").onclick = function(){saveName();}
 }
+*/
 
-
-
-</script>
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
